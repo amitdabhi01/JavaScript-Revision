d092c1aaf13599af824720fff43a32da3eef9c19
Dabhi Amit <amitdabhi526@gmail.com>
2026-02-12 12:35:21 +0530

    js started

---
+++ 01_add js in html/index.js
@@ -0,0 +1,5 @@
+document.getElementById("demo").textContent = "Click On Button";
+
+document.getElementById("btn").addEventListener("click", function myFunction() {
+  document.getElementById("btn").textContent = "Log In";
+});
