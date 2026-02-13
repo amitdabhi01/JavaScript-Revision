7c5fda88d224df359b7fd6a413ca75d6b3146862
Dabhi Amit <amitdabhi526@gmail.com>
2026-02-13 12:24:20 +0530

    some change

--- 01_add js in html/index.js
+++ 01_add js in html/index.js
@@ -3,3 +3,32 @@ document.getElementById("demo").textContent = "Click On Button";
 document.getElementById("btn").addEventListener("click", function myFunction() {
   document.getElementById("btn").textContent = "Log In";
 });
+
+
+// console.log("10" * "2");
+
+// console.log("20" / 2);
+
+// console.log(null + 1);
+
+// console.log("5" + true);
+
+// console.log("5" - true);
+
+// console.log(parseInt("100px"));
+
+// console.log(parseFloat("12.75kg"));
+
+// console.log(String(false));
+
+// console.log(Boolean(""));
+
+// console.log(Boolean("0"));
+
+// console.log(+"10" + 5);
+
+// console.log("4" + null);
+
+// console.log("4" - null);
+
+// console.log("16" / "4");
